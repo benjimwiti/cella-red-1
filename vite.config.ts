c268816ea78cbee83970475e2a9fecdee4fc2525
benjimwiti <167991485+benjimwiti@users.noreply.github.com>
2025-09-18 01:55:28 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-base: "/cella-red-1/main.tsx",
   server: {
     host: "::",
     port: 8080,
